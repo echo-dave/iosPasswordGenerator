fee583b05b729763b46aa8f4f7215863191f424f
Dave May <echo.dave@protonmail.com>
2021-10-28 16:24:22 -0400

    Basic working UI

--- Password Generator/ViewController.swift
+++ Password Generator/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var useUppercase: UISwitch!
+    @IBOutlet weak var useNumbers: UISwitch!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        useNumbers.setOn(true, animated: true)
+//        print(useNumbers.isOn)
     }
 
 
+//    @IBAction func useNumbersChange(_ sender: UISwitch) {
+//        print(useNumbers.isOn)
+//    }
+    
+    @IBAction func MakePassword(_ sender: UIButton) {
+        
+        let newPassword = randomize(useNumbers: useNumbers, useUppercase: useUppercase)
+        
+        print("new password: \(newPassword)")
+        print("password length: \(newPassword.count)")
+    }
 }
 
